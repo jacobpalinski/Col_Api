dea141684809bba76d730e37b509fd1ec80f4912
Kuba Palinski <kpalinski99@gmail.com>
2023-02-02 19:59:12 +0800

    Pagination Helper

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 # Replace your_password with the password you specified for the database
 SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Databrah91@5432/cost_of_living"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
-
+PAGINATION_PAGE_SIZE = 50
+PAGINATION_PAGE_ARGUMENT_NAME = 'page'
--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 from httpstatus import *
 from models import orm
 from flask import make_response
+from flask import url_for
+from flask import current_app
 
 def request_not_empty(dict):
     if dict == False:
@@ -21,3 +23,39 @@ def delete_object(object):
     object.delete(object)
     response = make_response()
     return response, HttpStatus.no_content_204.value
+
+class PaginationHelper():
+    def __init__(self,request, query, resource_for_url, key_name, schema):
+        self.request = request
+        self.query = query
+        self.resource_for_url = resource_for_url
+        self.key_name = key_name
+        self.schema = schema
+        self.page_size = current_app.config['PAGINATION_PAGE_SIZE']
+        self.page_argument_name = current_app.config['PAGINATION_PAGE_ARGUMENT_NAME']
+
+    def paginate_query(self):
+        # No page number, assume request requires page #1
+        page_number = self.request.args.get(self.page_argument_name, 1, type=int)
+        paginated_objects = self.query.paginate(
+            page_number,
+            per_page = self.page_size,
+            error_out = False
+        )
+        objects = paginated_objects.items
+        if paginated_objects.has_prev:
+            previous_page_url = url_for(self.resource_for_url,page = page_number - 1, _external = True)
+        else:
+            previous_page_url = None
+        if paginated_objects.has_next:
+            next_page_url = url_for(self.resource_for_url, page = page_number + 1, _external = True)
+        else:
+            next_page_url = None
+        
+        dumped_objects = self.schema.dump(objects, many = True)
+        return {
+            self.key_name : dumped_objects,
+            'previous' : previous_page_url,
+            'next' : next_page_url,
+            'count': paginated_objects.total
+        }
