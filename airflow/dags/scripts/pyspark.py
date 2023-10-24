66c3b71c4fe73bfcb9cdd1064226fb51aca9d445
root <root@LAPTOP-2Q577UFQ>
2023-10-24 19:40:20 +0800

    merge_and_transform_homepurchase

--- airflow/dags/scripts/pyspark.py
+++ airflow/dags/scripts/pyspark.py
@@ -19,53 +19,63 @@ country_abbreviation_combinations = {'United Arab Emirates': 'AED', 'Australia':
 # Pyspark session to be used by each transformation function
 pyspark = create_spark_session('col_api_etl')
 
-def merge_cities_with_currencies(spark_session: SparkSession, country_abbreviation_combinations : list):
-   # Retreive cities and currency_conversion_rates from S3 bucket
-   cities = get_data('cities')
-   currency_conversion_rates = get_data('currency_conversion_rates')
+def merge_locations_with_currencies(spark_session: SparkSession, country_abbreviation_combinations : list):
+   # Retrieve locations and currency_conversion_rates from S3 bucket
+   locations = get_data(file_prefix = 'locations.json')
+   currency_conversion_rates = get_data(file_prefix = 'currency_conversion_rates')
 
    # Convert data into lists of Row objects
-   cities_rows = [Row(**row) for row in cities]
+   locations_rows = [Row(**row) for row in locations]
    currency_conversion_rates_rows = [Row(**row) for row in currency_conversion_rates]
 
    # Create dataframes from row objects
-   cities_df = pyspark.createDataFrame(cities_rows)
+   locations_df = pyspark.createDataFrame(locations_rows)
    currency_conversion_rates_df = pyspark.createDataFrame(currency_conversion_rates_rows)
-
-   # Dictionary used to replace countries with None in City column in cities_df
-   country_capital_dict = {'Bahrain': 'Manama', 'Liechtenstein': 'Vaduz', 'Chile': 'Santiago', 'Costa Rica': 'San Jose', 'Andorra': 'Andorra la Vella', 'Estonia': 'Tallinn',
-   'Latvia': 'Riga', 'Luxembourg': 'Luxembourg','Malta': 'Valletta', 'Montenegro': 'Podgorica', 'Monaco': 'Monaco', 'San Marino': 'San Marino', 'Slovakia': 'Bratislava', 
-   'Slovenia': 'Ljubljana', 'Georgia':'Tbilisi', 'Hong Kong': 'Hong Kong', 'Iceland': 'Reykjavik', 'Kuwait': 'Kuwait City', 'Macau': 'Macau' ,'Oman': 'Muscat', 'Qatar': 'Doha', 
-   'Singapore': 'Singapore', 'Uruguay': 'Montevideo', 'Paraguay': 'Asuncion', 'Panama': 'Panama City', 'Argentina': 'Buenos Aires'}
-
-   # UDF to replace 'City' based on 'Country' using country_capital_dict
-   def replace_city(country, city):
-      return country_capital_dict.get(country, city)
-   
-   # Register the UDF
-   replace_city_udf = functions.udf(replace_city, StringType())
-
-   # Replace rows with None City column with country_capital_dict
-   cities_df = cities_df.withColumn('City', replace_city_udf(functions.col('Country'), functions.col('City')))
 
    # Create map based on country_abbreviation_combinations
    country_abbreviation_combinations_map = functions.create_map(*[functions.lit(combination) for combination in chain(*country_abbreviation_combinations.items())])
 
    # Add Abbreviation column using country_abbreviation_combinations
-   cities_df = cities_df.withColumn('Abbreviation', functions.when(functions.col('Country').isin(list(country_abbreviation_combinations.keys())), 
+   locations_df = locations_df.withColumn('Abbreviation', functions.when(functions.col('Country').isin(list(country_abbreviation_combinations.keys())), 
    country_abbreviation_combinations_map[functions.col('Country')]))
 
    # Remove commas from USD_to_local and converting to floating type
    currency_conversion_rates_df = currency_conversion_rates_df.withColumn('USD_to_local', functions.regexp_replace(functions.col('USD_to_local'), ',', '').cast(FloatType()))
 
    # Join dataframes
-   cities_with_currencies_df = cities_df.join(currency_conversion_rates_df, 'Abbreviation', 'inner')
+   locations_with_currencies_df = locations_df.join(currency_conversion_rates_df, 'Abbreviation', 'inner')
 
    # Convert to list of dictionaries
-   cities_with_currencies = [{**row.asDict(), 'USD_to_local': round(row.USD_to_local, 2)} for row in cities_with_currencies_df.collect()]
+   locations_with_currencies = [{**row.asDict(), 'USD_to_local': round(row.USD_to_local, 2)} for row in locations_with_currencies_df.collect()]
 
    # Load data to S3 transformed bucket
-   put_data(file_prefix = 'cities_with_currencies', data = cities_with_currencies, bucket_type = 'transformed')
+   put_data(file_prefix = 'locations_with_currencies', data = locations_with_currencies, bucket_type = 'transformed')
+
+def merge_and_transform_homepurchase(spark_session: SparkSession):
+   # Retrieve numbeo_price_info from S3 bucket
+   numbeo_price_info = get_data(file_prefix = 'numbeo_price_info')
+
+   # Convert data into lists of Row objects
+   numbeo_price_info_rows = [Row(**row) for row in numbeo_price_info]
+
+   # Create dataframes from row objects
+   numbeo_price_info_df = pyspark.createDataFrame(numbeo_price_info_rows)
+
+   # Filter criterea
+   items = ['Price per Square Meter to Buy Apartment in City Centre', 'Price per Square Meter to Buy Apartment Outside of Centre']
+
+   # Filter homepurchase items
+   numbeo_price_info_homepurchase_df = numbeo_price_info_df.filter(functions.col('Item').isin(items))
+
+   # Format price string and convert to float
+   numbeo_price_info_homepurchase_df = numbeo_price_info_homepurchase_df.withColumn('Price', functions.regexp_replace(functions.col('Price'), r'[^0-9.]', ''))
+   numbeo_price_info_homepurchase_df = numbeo_price_info_homepurchase_df.withColumn('Price', functions.col('Price').cast('float'))
+
+   # Convert to list of dictionaries
+   homepurchase = [{**row.asDict(), 'Price': round(row.Price, 2)} for row in numbeo_price_info_homepurchase_df.collect()]
+
+   # Load data to S3 transformed bucket
+   put_data(file_prefix = 'homepurchase', data = homepurchase, bucket_type = 'transformed')
 
 
     
--- airflow/dags/tests/test_pyspark.py
+++ airflow/dags/tests/test_pyspark.py
@@ -8,18 +8,32 @@ from utils.aws_utils import get_data
 from tests.fixtures.fixtures_testing import mock_environment_variables, mock_boto3_s3, pyspark_session, current_date
 from scripts.pyspark import *
 
-def test_merge_cities_and_currencies(mock_environment_variables, mock_boto3_s3, pyspark_session, current_date, mocker):
-   merge_cities_with_currencies(spark_session = pyspark_session, country_abbreviation_combinations = country_abbreviation_combinations)
-   expected_get_object_calls = [call(Bucket='test-bucket-raw', Key=f'cities{current_date}'),
-   call(Bucket='test-bucket-raw', Key=f'currency_conversion_rates{current_date}')]
-   expected_cities_with_currencies = json.dumps([{"Abbreviation": "AUD", "Country": "Australia", "City": "Perth", "USD_to_local": 1.55},
+def test_merge_locations_and_currencies(mock_environment_variables, mock_boto3_s3, pyspark_session, current_date, mocker):
+   merge_locations_with_currencies(spark_session = pyspark_session, country_abbreviation_combinations = country_abbreviation_combinations)
+   expected_get_object_calls = [call(Bucket = 'test-bucket-raw', Key = 'locations.json'),
+   call(Bucket = 'test-bucket-raw', Key = f'currency_conversion_rates{current_date}')]
+   expected_locations_with_currencies = json.dumps([{"Abbreviation": "AUD", "Country": "Australia", "City": "Perth", "USD_to_local": 1.55},
+   {"Abbreviation": "HKD", "Country": "Hong Kong", "City": "Hong Kong", "USD_to_local": 7.82},
    {"Abbreviation": "NZD", "Country": "New Zealand", "City": "Auckland", "USD_to_local": 1.69}, 
-   {"Abbreviation": "PYG", "Country": "Paraguay", "City": "Asuncion", "USD_to_local": 7258.93}, 
-   {"Abbreviation": "SGD", "Country": "Singapore", "City": "Singapore", "USD_to_local": 1.36}])
+   {"Abbreviation": "PYG", "Country": "Paraguay", "City": "Asuncion", "USD_to_local": 7258.93}])
    assert mock_boto3_s3.get_object.call_count == 2
    assert mock_boto3_s3.get_object.call_args_list == expected_get_object_calls
-   mock_boto3_s3.put_object.assert_called_once_with(Bucket = 'test-bucket-transformed', Key = f'cities_with_currencies{current_date}',
-   Body = expected_cities_with_currencies)
+   mock_boto3_s3.put_object.assert_called_once_with(Bucket = 'test-bucket-transformed', Key = f'locations_with_currencies{current_date}',
+   Body = expected_locations_with_currencies)
+
+def test_merge_and_transform_homepurchase(mock_environment_variables, mock_boto3_s3, pyspark_session, current_date, mocker):
+   merge_and_transform_homepurchase(spark_session = pyspark_session)
+   expected_homepurchase = json.dumps([{"City": "Perth", "Item": "Price per Square Meter to Buy Apartment in City Centre", "Price": 6741.52},
+   {"City": "Perth", "Item": "Price per Square Meter to Buy Apartment Outside of Centre", "Price": 5395.77},
+   {"City": "Auckland", "Item": "Price per Square Meter to Buy Apartment in City Centre", "Price": 9155.42},
+   {"City": "Auckland", "Item": "Price per Square Meter to Buy Apartment Outside of Centre", "Price": 8089.96},
+   {"City": "Hong Kong", "Item": "Price per Square Meter to Buy Apartment in City Centre", "Price": 30603.04},
+   {"City": "Hong Kong", "Item": "Price per Square Meter to Buy Apartment Outside of Centre", "Price": 20253.04},
+   {"City": "Asuncion", "Item": "Price per Square Meter to Buy Apartment in City Centre", "Price": 1118.53},
+   {"City": "Asuncion", "Item": "Price per Square Meter to Buy Apartment Outside of Centre", "Price": 933.23}])
+   mock_boto3_s3.get_object.assert_called_once_with(Bucket = 'test-bucket-raw', Key = f'numbeo_price_info{current_date}')
+   mock_boto3_s3.put_object.assert_called_once_with(Bucket = 'test-bucket-transformed', Key = f'homepurchase{current_date}',
+   Body = expected_homepurchase)
 
 
 
